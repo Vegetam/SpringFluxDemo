d57031ae0275e19f7ffbba9421f06b6e0c73dda2
Francesco Malagrino <francesco.malagrino@gmail.com>
2024-09-26 21:14:45 +0200

    Initial Structure

---
+++ src/test/java/com/francescomalagrino/webflux/Webflux_demo/WebfluxDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.francescomalagrino.webflux.Webflux_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
